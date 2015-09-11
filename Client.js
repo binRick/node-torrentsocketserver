8c70991230284eecd7811323c7c71df7ad82a141
Rick <rick@rick.local>
2015-09-11 13:19:23 -0600

    iiwq

---
+++ Client.js
@@ -0,0 +1,41 @@
+var fs = require('fs'),
+    pb = require('pretty-bytes'),
+    c = require('chalk'),
+    Spinner = require('node-spinner'),
+    io = require('socket.io-client'),
+    ss = require('socket.io-stream'),
+    clear = require('cli-clear'),
+    async = require('async'),
+    _ = require('underscore');
+var magnet = process.argv[2];
+host = process.argv[3]||'localhost';
+var port = process.argv[4]||4911;
+var spin = new Spinner();
+
+
+
+var socket = io.connect('http://'+host+':'+port+'/fileRequest');
+var iS = null;
+var sent = 0;
+socket.emit('magnet', magnet, function(err, Files) {
+    if (err) throw err;
+    console.log('Files', Files);
+    _.each(Files, function(file) {
+        var dFile = __dirname + '/dFile_' + file.name;
+        console.log('requesting stream for file ', file, 'to', dFile);
+        var stream = ss.createStream();
+        stream.on('data', function(chunk) {
+            sent = chunk.length + sent;
+            var m = '\r' + c.green('chunked') + ' ' + spin.next();
+            process.stdout.write(m);
+        });
+        stream.on('end', function() {
+            console.log('\nstream ended. sent ', pb(sent));
+        });
+        ss(socket).emit('streamMagnetFile', stream, {
+            magnet: magnet,
+            file: file
+        });
+        stream.pipe(fs.createWriteStream(dFile));
+    });
+});
---
+++ Server.js
@@ -0,0 +1,72 @@
+var fs = require('fs'),
+    c = require('chalk');
+var _ = require('underscore');
+host =process.argv[2]||'127.0.0.1';
+var port = process.argv[3]||4911;
+var io = require('socket.io').listen(port);
+var ss = require('socket.io-stream');
+var path = require('path');
+var file = __dirname + '/file';
+var torrentStream = require('torrent-stream');
+var spinner = require('node-spinner');
+var iS = null;
+
+
+io.of('/fileRequest').on('connection', function(socket) {
+    console.log('socket ev');
+	    socket.on('end', function(){
+		console.log('SOCKET ENDED');    
+    });
+    socket.on('magnet', function(magnet, _cb) {
+        var Spin = new spinner();
+        var m = c.green.dim('Magnet -> Files request received. Connecting to torrent. ');
+        iS = setInterval(function() {
+            process.stdout.write('\r' + m + c.blue(Spin.next()));
+        }, 100);
+        var engine = torrentStream(magnet);
+        engine.on('ready', function() {
+            clearInterval(iS);
+            process.stdout.write('\n');
+            var m = 'Magnet Ready. Sending Files ';
+            var Files = engine.files.map(function(f) {
+                return {
+                    name: f.name,
+                    path: f.path,
+                    length: f.length,
+                };
+            });
+            _cb(null, Files);
+            process.stdout.write('\nSent ' + Files.length + '\n');
+        });
+    });
+    ss(socket).on('streamMagnetFile', function(stream, data) {
+        var engine = torrentStream(data.magnet);
+        console.log('connecting to', data.magnet.length, ' ');
+        engine.on('ready', function() {
+            console.log('magnet', data.magnet.length, 'ready');
+            var sentBytes = 0;
+            engine.files.forEach(function(file) {
+		    if(file.name!=data.file.name)return;
+		console.log('sending file', file.name);
+		var torrentStream = file.createReadStream();
+		torrentStream.on('upload', function(index, offset, length){
+                    console.log('uploaded index ', index, 'offset', offset, 'length', length);
+		});
+		torrentStream.on('download', function(index){
+                    console.log('downloaded index ', index);
+		});
+		torrentStream.on('data', function(chunk){
+                    sentBytes = sentBytes + chunk.length;
+                    console.log('sent ', chunk.length, 'total sent:', sentBytes,'file size', file.length);
+		});
+                torrentStream.on('end', function() {
+                    console.log('stream ended. total sent: ', sentBytes);
+                });
+                torrentStream.on('start', function() {
+                    console.log('start');
+                });
+		torrentStream.pipe(stream);
+	    });
+        });
+    });
+});
